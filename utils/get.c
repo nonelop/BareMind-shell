7a19260adfdaaa455dc2a551e782562fd4645985
nonelop <morlksaq@gmail.com>
2026-06-13 20:16:05 +0700

    Great

--- utils/get.c
+++ utils/get.c
@@ -13,6 +13,8 @@ int get(char* tokens[], int tokens_count) {
 
     for (int i = 0; i < tokens_count; i++) {
         if (strcmp(tokens[i], "get") != 0) {
+
+            // flags
             if (startswith(tokens[i], "-")) {
                 switch (tokens[i][1]) {
                     case ('l'):
@@ -31,37 +33,49 @@ int get(char* tokens[], int tokens_count) {
                         break;
                  }
             }
+
+            // models
             else {
                 int is_exist = 0;
+                int n;
                 for (int j = 0; j < models_count; j++) {
                     if (!strcmp(models_list[j]->short_name, tokens[i])) {
-                        char* size_in = "MB";
-                        Model* model = models_list[j];
-                        float model_size = model->size;
-
-                        if (model->size >= 1024) {
-                            model_size = model->size / 1024;
-                            size_in = "GB";
-                        }
-
-                        printf("\n%s\n\n", model->full_name);
-                        printf("MODEL SPECIFICATIONS\n[QUANT]: %s\n[SIZE]: %.2f%s\n\n", model->quantization, model_size, size_in);
-
-                        char* input_buffer = linenoise("Are you sure you want to install this model? [y / n] ~$: ");
-
-                        if (!strcmp(input_buffer, "y") || !strcmp(input_buffer, "Y") || !strcmp(input_buffer, "")) {
-                            char command[256];
-                            sprintf(command, "curl -L -o ./models/%s.gguf %s", model->full_name, model->download_url);
-
-                            system(command);
-                        }
-                        operation_successful = 1;
                         is_exist = 1;
+                        n = j;
                     }
+                    else if (!strcmp(models_list[j]->full_name, tokens[i])) {
+                        is_exist = 1;
+                        n = j;
+                    }
+
                     else if (j == models_count - 1 && is_exist == 0) {
                         printf("Model with this name does not exist.\n");
                         operation_successful = 1;
                     }
+                }
+
+                if (is_exist == 1) {
+                    char* size_in = "MB";
+                    Model* model = models_list[n];
+                    float model_size = model->size;
+
+                    if (model->size >= 1024) {
+                        model_size = model->size / 1024;
+                        size_in = "GB";
+                    }
+
+                    printf("\n%s\n", model->full_name);
+                    printf("Specifications:\n[QUANT]: %s\n[SIZE]: %.2f%s\n\n", model->quantization, model_size, size_in);
+
+                    char* input_buffer = linenoise("Are you sure you want to install this model? [y / n] ~$: ");
+
+                    if (!strcmp(input_buffer, "y") || !strcmp(input_buffer, "Y") || !strcmp(input_buffer, "")) {
+                        char command[256];
+                        sprintf(command, "curl -L -o ./models/%s.gguf %s", model->full_name, model->download_url);
+
+                        system(command);
+                    }
+                    operation_successful = 1;
                 }
             }
         }
